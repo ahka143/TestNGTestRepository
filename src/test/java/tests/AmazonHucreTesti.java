c7bc3e7027e7c5fea8b31df4394bc2c89e2f2f34
Ahmet Kaya <ahka1143@gmail.com>
2022-06-05 23:28:13 +0300

    05.06.2022-1

--- src/test/java/tests/AmazonHucreTesti.java
+++ src/test/java/tests/AmazonHucreTesti.java
@@ -1,28 +1,33 @@
 package tests;
 
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.AmazonPage;
+import utilities.ConfigReader;
 import utilities.Driver;
 
 public class AmazonHucreTesti {
-AmazonPage amazonPage;
+    AmazonPage amazonPage;
 
     @Test
-    public void testName() {
-        //webtable’da 3. satir 2.sutundaki yazinin “Home Services” yazisi
+    public void hucretesti()  {
+        //webtable’da 3. satir 7.sutundaki yazinin “Home Services” yazisi
         //icerdigini test edin
-        amazonPage=new AmazonPage();
-
-
+        amazonPage = new AmazonPage();
+        Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
+        String expectedStr = "Home Services";
+        String actualStr = hucredekiDatayiBul(3, 7);
+        Assert.assertTrue(actualStr.contains(expectedStr));
+        Driver.closeDriver();
 
     }
 
     public String hucredekiDatayiBul(int satir, int sutun) {
-amazonPage=new AmazonPage();
+        amazonPage = new AmazonPage();
         //AmazonPage sayfasinda istedigim satir ve sutun sayisi ile cagirdigimda
         //bana hucredeki yaziyi getirecek bir method olusturun
 
-        return   Driver.getDriver().findElement(By.xpath("//tr["+satir+"]//td["+sutun+"]")).getText();
+        return Driver.getDriver().findElement(By.xpath("//tr[" + satir + "]//td[" + sutun + "]")).getText();
     }
 }
